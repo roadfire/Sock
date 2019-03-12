a3b62abf2a17ce4f183d200c16951cc161a5decb
Matthew Lorentz <lorentz_matt@network.lilly.com>
2019-03-12 09:52:06 -0400

    Initial Commit

---
+++ Sock/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Sock
+//
+//  Created by Matt Lorentz on 3/12/19.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Sock/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Sock
+//
+//  Created by Matt Lorentz on 3/12/19.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
